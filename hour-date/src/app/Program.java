2f1342ac65cb751e9b3c322eb66ba27022dc598e
Hendel Fonseca <hendelfonseca@id.uff.br>
2022-09-19 16:01:20 -0300

    hours

--- hour-date/src/app/Program.java
+++ hour-date/src/app/Program.java
@@ -1,5 +1,6 @@
 package app;
 
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -7,6 +8,17 @@ public class Program {
 
     public static void main(String[] args) {
 
+    LocalDate d01 = LocalDate.now();
+    LocalDateTime d02 = LocalDateTime.now(); //
+    Instant d03 = Instant.now();
 
+    LocalDate d04 = LocalDate.parse("2022-09-19");
+    LocalDateTime d05 = LocalDateTime.parse("2022-09-19T01:08:26");
+
+    System.out.println("do1 = " + d01);
+    System.out.println("d02 = " + d02);
+    System.out.println("d03 = " + d03);
+    System.out.println("do4 = " + d04);
+    System.out.println("do5 = " + d05);
     }
 }
