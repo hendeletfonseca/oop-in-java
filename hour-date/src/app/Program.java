974c23a298f1ef07b577cf97d040791582e0fb02
Hendel <hendelfonseca@id.uff.br>
2022-09-26 10:14:28 -0300

    enums

--- hour-date/src/app/Program.java
+++ hour-date/src/app/Program.java
@@ -15,10 +15,10 @@ public class Program {
     LocalDate d04 = LocalDate.parse("2022-09-19");
     LocalDateTime d05 = LocalDateTime.parse("2022-09-19T01:08:26");
 
-    System.out.println("do1 = " + d01);
-    System.out.println("d02 = " + d02);
-    System.out.println("d03 = " + d03);
-    System.out.println("do4 = " + d04);
-    System.out.println("do5 = " + d05);
+    System.out.println("do1 = " + d01.toString());
+    System.out.println("d02 = " + d02.toString());
+    System.out.println("d03 = " + d03.toString());
+    System.out.println("do4 = " + d04.toString());
+    System.out.println("do5 = " + d05.toString());
     }
 }
